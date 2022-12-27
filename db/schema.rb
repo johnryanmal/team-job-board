5e3fdcf1f9d36635537d2a4240c408e984dca7ae
“johnryanmal” <johnryanmal@gmail.com>
2022-12-27 11:30:56 -0800

    associate many tags to one job

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_191011) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_192956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_191011) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "job_id"
   end
 
   create_table "users", force: :cascade do |t|
