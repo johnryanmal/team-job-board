24987986c52db44cd74cb096df94475981cc8cd0
“johnryanmal” <johnryanmal@gmail.com>
2022-12-29 12:56:07 -0800

    make jobs searchable by tag

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,7 +1,7 @@
 class GeneralController < ApplicationController
   def search
     @query = params[:query]
-    @jobs = Job.ransack(title_or_description_cont: @query).result(distinct: true)
+    @jobs = Job.ransack(title_or_description_or_tags_name_cont: @query).result(distinct: true).includes(:tags)
     @companies = Company.ransack(name_or_description_cont: @query).result(distinct: true)
   end
 end
