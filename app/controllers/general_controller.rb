3cdc86a1ab387558a98d2d299efecdf0bf40656b
“johnryanmal” <johnryanmal@gmail.com>
2022-12-29 10:49:14 -0800

    link search button to general search

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,5 +1,7 @@
 class GeneralController < ApplicationController
   def search
-
+    @query = params[:query]
+    @jobs = Job.ransack(title_or_description_cont: @query).result(distinct: true)
+    @companies = Company.ransack(name_or_description_cont: @query).result(distinct: true)
   end
 end
